6f174ca65e8046ec8952f0d03d53550b889c79bb
Olga Vanyushkina <jilliya13@gmail.com>
2018-01-23 00:35:23 +0300

    Оптимизация переходов

--- fixture/contact.py
+++ fixture/contact.py
@@ -46,7 +46,8 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith(" ") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
