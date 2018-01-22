a70c40e73966fb5defbc652e8cf93e90f2f0c1fb
Olga Vanyushkina <jilliya13@gmail.com>
2018-01-22 22:22:51 +0300

    Интеллектуальная фикстура

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Anna", lastname="Orlova", mobile="64985587"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", mobile=""))
-    app.session.logout()
+
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,12 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_first_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="Masha"))
-    app.session.logout()
 
 
 def test_modify_first_contact_lastname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(lastname="Semenova"))
-    app.session.logout()
