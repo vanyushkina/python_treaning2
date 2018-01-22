bd8ade71a112cff54ec0860a0376055703407ca1
Olga Vanyushkina <jilliya13@gmail.com>
2018-01-23 00:20:12 +0300

    Обеспечение выполнения предусловий

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,8 +2,12 @@ from model.contact import Contact
 
 
 def test_modify_first_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Nick"))
     app.contact.modify_first_contact(Contact(firstname="Masha"))
 
 
 def test_modify_first_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(lastname="Frolov"))
     app.contact.modify_first_contact(Contact(lastname="Semenova"))
