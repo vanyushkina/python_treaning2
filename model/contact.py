525d70f03f6d3cbc40b90bbe5a3a60abdb04dcc3
Olga Vanyushkina <jilliya13@gmail.com>
2018-01-23 04:41:29 +0300

    Сравнение списков групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -5,3 +5,9 @@ class Contact:
         self.lastname = lastname
         self.mobile = mobile
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.firstname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname
